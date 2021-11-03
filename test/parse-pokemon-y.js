af8250e1d2089b95c7348379ae642b087d1d7a43
Jonathan Barrow <jonbarrow1998@gmail.com>
2021-11-03 08:44:49 -0400

    Export all DDL types

--- test/parse-pokemon-y.js
+++ test/parse-pokemon-y.js
@@ -1,6 +1,13 @@
 const DDL = require('..');
+const ESSerializer = require('esserializer');
+const fs = require('fs');
 const pokemonDumpPath = `${__dirname}/../pokemony/exefs/code.bin`;
 
 const trees = DDL.parse(pokemonDumpPath);
 
-console.log(trees);
+for (const tree of trees) {
+	const serialized = ESSerializer.serialize(tree, {
+		ignoreProperties: ['fd']
+	});
+	fs.writeFileSync(`${__dirname}/pokemon-y-tree-${trees.indexOf(tree)}.json`, JSON.stringify(JSON.parse(serialized), null, 4)); // write pretty-printed
+}
